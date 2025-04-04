159edc1bdeca6c50c2578c124c1ae73be72f65c3
imedbouajila <bouajila-imed@hotmail.com>
2025-04-04 11:31:22 +0200

    check on 04-04-2025

--- troubleshooting.sh
+++ troubleshooting.sh
@@ -6,5 +6,5 @@ sudo systemctl restart kube-proxy
 ssh -OPTIONS -p SSH_PORT user@remote_server "remote_command1; remote_command2; remote_script.sh"
 ssh -p 22 devops@node1 "sudo systemctl restart kubelet"
 
-#check connectivity via
+##check connectivity via
 worker# nc -vz 192.168.1.230 6443
